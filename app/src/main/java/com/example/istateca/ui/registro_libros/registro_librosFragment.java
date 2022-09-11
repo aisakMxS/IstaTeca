20bfd4c8883e4dfe5fff92818e873b2b88ce9186
Christian1910n <christian.garaicoa.est@tecazuay.edu.ec>
2022-09-11 15:56:41 -0500

    Libro Avance

--- app/src/main/java/com/example/istateca/ui/registro_libros/registro_librosFragment.java
+++ app/src/main/java/com/example/istateca/ui/registro_libros/registro_librosFragment.java
@@ -20,6 +20,7 @@ import com.example.istateca.Utils.Apis;
 import com.example.istateca.Utils.LibroService;
 import com.example.istateca.databinding.FragmentRegistroLibrosBinding;
 
+import java.sql.Time;
 import java.sql.Timestamp;
 
 import retrofit2.Call;
@@ -48,10 +49,38 @@ public class registro_librosFragment extends Fragment {
                 String inputString = "Hello World!";
                 byte[] byteArrray = inputString.getBytes();
 
+                String codigoDewey=binding.txtCodigodewey.getText().toString();
+                String titulo=binding.txtTituloLlibro.getText().toString();
+                int tipo= Integer.parseInt(binding.txtTipo.getText().toString());
+                String adquisicion= binding.txtAdquisicionLibro.getText().toString();
+                int anio= Integer.parseInt(binding.txtAnioPublicacion.getText().toString());
+                String editor=binding.txtEditor.getText().toString();
+                String ciudad= binding.txtCiudad.getText().toString();
+                int numpaginas=Integer.parseInt(binding.txtNumeroPaginas.getText().toString());
+                String area= binding.txtArea.getText().toString();
+                String codisbn= binding.txtCodigoIsbn.getText().toString();
+                String idioma=binding.txtIdioma.getText().toString();
+                String descripcion= binding.txtDescripcion.getText().toString();
+                String in1= "Indice 1";
+                String in2= "Indice 2";
+                String in3= "Indice 3";
+                String dimensiones=binding.txtDimensiones.getText().toString();
+                String estadolibro=binding.txtEstadoLibro.getText().toString();
+                Boolean activo=true;
+                byte[] imagen = null;
+                String url=  binding.txtUrl.getText().toString();
+                int idBibliotecario= Integer.parseInt(binding.txtBibliotecarioRegistra.getText().toString());
+                Timestamp fecha= null;
+                boolean disponibilidad= false;
+                String donante= binding.txtAnadirDonante.getText().toString();
+                byte[] documentodonacion= null;
 
 
-                Libro l = new Libro(5,"Dewey","El chema",1,"adquisicionqwe",1980,"Editort","Cuenca", 90, "Area", "Isbn123"
-                        , "Español", "Descripcion aasfa", "IUno", "IDos","Itres","Dimensiones", "Estado", false,null,"asfasdURL",1,null,true,"Christian",null);
+                //Libro l = new Libro(5,"Dewey","El chema",1,"adquisicionqwe",1980,"Editort","Cuenca", 90, "Area", "Isbn123"
+                  //      , "Español", "Descripcion aasfa", "IUno", "IDos","Itres","Dimensiones", "Estado", false,null,"asfasdURL",1,null,true,"Christian",null);
+
+                Libro l = new Libro(5,codigoDewey,titulo,tipo,adquisicion,anio,editor,ciudad,numpaginas,area,codisbn,idioma,descripcion,
+                        in1,in2,in3,dimensiones,estadolibro,activo,imagen,url,idBibliotecario,fecha,disponibilidad,donante,documentodonacion);
 
                 create(l);
 
@@ -65,7 +94,7 @@ public class registro_librosFragment extends Fragment {
     }
 
     private void create(Libro l){
-        Retrofit retrofit= new Retrofit.Builder().baseUrl("http://10.0.2.2:8080/api/").addConverterFactory(GsonConverterFactory.create()).build();
+        Retrofit retrofit= new Retrofit.Builder().baseUrl("http://192.168.68.110:8080/api/").addConverterFactory(GsonConverterFactory.create()).build();
         libroService= retrofit.create(LibroService.class);
         Call<Libro> call= libroService.addlibro(l);
         call.enqueue(new Callback<Libro>() {
