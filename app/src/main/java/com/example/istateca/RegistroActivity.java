dc6508f014de3b091a9d241b3c7490492eea20d2
BS <byron.siguenza.est@tecazuay.edu.ec>
2022-09-21 23:40:29 -0500

    Registerm0

--- app/src/main/java/com/example/istateca/RegistroActivity.java
+++ app/src/main/java/com/example/istateca/RegistroActivity.java
@@ -31,21 +31,24 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class RegistroActivity extends AppCompatActivity  implements View.OnTouchListener
-{
+public class RegistroActivity extends AppCompatActivity {
 
     private UsuarioService usuarioService;
-    String url="http://10.0.2.2:8080/api/";
+    /*private static final String url="http://192.168.18.14:4200/api/";*/
+    /*String Pced = txtcedula.getText().toString().trim();*/
+    private static final String url="http://192.168.18.14:4200/api/fenix_alumno?ced";
     ActivityRegistroBinding binding;
 
-    EditText txtcedula;
-    EditText txtnombres;
-    EditText txtusuario;
-    EditText txtcelular;
-    EditText txtcorreo;
-    EditText txtclave;
-    Button btnregister, btnSearch;
+    private static EditText txtcedula;
+    private static EditText txtnombres;
+    private static EditText txtusuario;
+    private static EditText txtcelular;
+    private static EditText txtcorreo;
+    private static EditText txtclave;
+    private static Button btnregister, btnSearch;
     Context context;
+/*  StringRequest stringRequest;
+    RequestQueue requestQueue;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +56,7 @@ public class RegistroActivity extends AppCompatActivity  implements View.OnTouch
         binding = ActivityRegistroBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         usuarioService = Apis.getUsuarioService();
+        /*requestQueue = Volley.newRequestQueue(this);*/
 
         btnSearch=findViewById(R.id.btnBuscar);
         btnSearch.setOnClickListener(new View.OnClickListener() {
@@ -74,8 +78,7 @@ public class RegistroActivity extends AppCompatActivity  implements View.OnTouch
 
     }
 
-
-    public void eventcedula(){
+/*    public void eventcedula(){
 
         txtcedula.setOnKeyListener(new View.OnKeyListener(){
 
@@ -85,56 +88,13 @@ public class RegistroActivity extends AppCompatActivity  implements View.OnTouch
                  return false;
             }
         });
-    }
-    public void getUsu(){
-        Retrofit retrofit= new Retrofit.Builder()
-                .baseUrl(url).addConverterFactory(GsonConverterFactory
-                        .create())
-                .build();
-        usuarioService= retrofit.create(UsuarioService.class);
-        Call<Persona> call= usuarioService.getUsuario(txtcedula.getText().toString());
-        call.enqueue(new Callback<Persona>() {
-            @Override
-            public void onResponse(Call<Persona> call, Response<Persona> response) {
-                if (!response.isSuccessful()){
-                    Log.e("Error: ",response.message());
-                    return;
-                }
-                String cedu = txtcedula.getText().toString().trim();
-                String nomb = txtnombres.getText().toString().trim();
-                String usua = txtusuario.getText().toString().trim();
-                String celu = txtcelular.getText().toString().trim();
-                String corr = txtcorreo.getText().toString().trim();
-                String clav = txtclave.getText().toString().trim();
+    }*/
 
-                if(cedu.isEmpty()||nomb.isEmpty()||usua.isEmpty()||
-                        celu.isEmpty()||corr.isEmpty()||clav.isEmpty()){
-                    Toast.makeText(context, "Required", Toast.LENGTH_SHORT).show();
-                    return;
 
-                }else{
-                    cedu = response.body().getCedula();
-                    nomb = response.body().getNombres();
-                    usua = response.body().getUsuario();
-                    celu = response.body().getCedular();
-                    corr = response.body().getCorreo();
-                    clav = response.body().getClave();
-                }
-
-/*              Persona person = response.body();
-                System.out.println(person);*/
-            }
-            @Override
-            public void onFailure(Call<Persona> call, Throwable t) {
-                Log.e("response","fail");
-            }
-        });
-    }
-
-    private  void openMain(){
+/*    private  void openMain(){
         Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
-    }
+    }*/
 
     /*public void getCed(String c){
         Retrofit retrofit= new Retrofit.Builder()
@@ -166,26 +126,7 @@ public class RegistroActivity extends AppCompatActivity  implements View.OnTouch
 
     }*/
 
-   /* public void initComponents(){
-        txtcedula = (EditText) findViewById(R.id.txt_cedula);
-        txtnombres= (EditText) findViewById(R.id.txt_nombres);
-        txtusuario= (EditText) findViewById(R.id.txt_usuario);
-        txtcelular= (EditText) findViewById(R.id.txt_celular);
-        txtcorreo= (EditText) findViewById(R.id.txt_correo);
-        txtclave= (EditText) findViewById(R.id.txt_clave);
-
-        *//*btnregister.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                requestRegister();
-            }
-        });*//*
-
-
-    }*/
-
-    @Override
+/*    @Override
     public boolean onTouch(View v, MotionEvent event) {
         if (event.getAction()==MotionEvent.ACTION_DOWN){
             txtnombres.setText("kevin Landivar");
@@ -198,33 +139,108 @@ public class RegistroActivity extends AppCompatActivity  implements View.OnTouch
             txtnombres.setText("vamos");
         }
         return false;
-    }
+    }*/
 
-/*    public void requestRegister(){
-        usuarioService.getUsuarioByCedula(txtcedula.getText().toString())
-                .enqueue(new Callback<Persona>() {
+
+/*    public void VieDate( View view){
+        String Pced = txtcedula.getText().toString().trim();
+        String url="http://192.168.18.14:4200/api/fenix_alumno?ced="+Pced;
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new com.android.volley.Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(Call<Persona> call, Response<Persona> response) {
-                        if(response.isSuccessful()){
-                            Log.i("debug","onResponse: Error");
-                            return;
-                            JSONObject json= new JSONObject(response.body().toString());
-                            try {
-                                json.put("name",txtcedula);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
+                    public void onResponse(JSONObject response) {
+                        String txtnom, txtcel, txtcor;
+                        try {
+                            txtnom = response.getString("nombres");
+                            txtcel = response.getString("celular");
+                            txtcor = response.getString("correo");
+
+                            txtnombres.setText(txtnom);
+                            txtcelular.setText(txtcel);
+                            txtcorreo.setText(txtcor);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
                         }
                     }
-
+                },
+                new com.android.volley.Response.ErrorListener() {
                     @Override
-                    public void onFailure(Call<Persona> call, Throwable t) {
-
+                    public void onErrorResponse(VolleyError error) {
+                        txtnombres.setText("");
+                        txtcelular.setText("");
+                        txtcorreo.setText("");
+                        Toast.makeText(RegistroActivity.this, "Usuario no registrado", Toast.LENGTH_SHORT).show();
                     }
-                });
-
-
+                }
+        );
+        requestQueue.add(jsonObjectRequest);
     }*/
+
+    public void getUsu(){
+        Retrofit retrofit= new Retrofit.Builder()
+                .baseUrl(url).addConverterFactory(GsonConverterFactory
+                        .create())
+                .build();
+        usuarioService= retrofit.create(UsuarioService.class);
+        Call<Persona> call= usuarioService.getUsuario(txtcedula.getText().toString());
+        call.enqueue(new Callback<Persona>() {
+            @Override
+            public void onResponse(Call<Persona> call, Response<Persona> response) {
+                if (!response.isSuccessful()){
+                    Log.e("Error: ",response.message());
+                    return;
+                }
+                String txtnom, txtcel, txtcor;
+                try {
+                    txtnom = response.body().getNombres();
+                    txtcel = response.body().getCedular();
+                    txtcor = response.body().getCorreo();
+
+                    txtnombres.setText(txtnom);
+                    txtcelular.setText(txtcel);
+                    txtcorreo.setText(txtcor);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+/*                String cedu = txtcedula.getText().toString().trim();
+                String nomb = txtnombres.getText().toString().trim();
+                String usua = txtusuario.getText().toString().trim();
+                String celu = txtcelular.getText().toString().trim();
+                String corr = txtcorreo.getText().toString().trim();
+                String clav = txtclave.getText().toString().trim();
+
+                if(cedu.isEmpty()||nomb.isEmpty()||usua.isEmpty()||
+                        celu.isEmpty()||corr.isEmpty()||clav.isEmpty()){
+                    Toast.makeText(context, "Required", Toast.LENGTH_SHORT).show();
+                    return;
+
+                }else{
+                    cedu = response.body().getCedula();
+                    nomb = response.body().getNombres();
+                    usua = response.body().getUsuario();
+                    celu = response.body().getCedular();
+                    corr = response.body().getCorreo();
+                    clav = response.body().getClave();
+                }*/
+
+/*             Persona person = response.body();
+                System.out.println(person);*/
+            }
+            @Override
+            public void onFailure(Call<Persona> call, Throwable t) {
+                txtnombres.setText("");
+                txtcelular.setText("");
+                txtcorreo.setText("");
+                Log.e("response","fail");
+            }
+        });
+    }
 
 
 }
