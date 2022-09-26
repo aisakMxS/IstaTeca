7a7e2b04c0164a5b6a106b3dfbd880823a366d00
Kevin Landivar <kevin.landivar.est@tecazuay.edu.ec>
2022-09-26 17:43:13 -0500

    karen

--- app/src/main/java/com/example/istateca/Utils/BibliotecarioService.java
+++ app/src/main/java/com/example/istateca/Utils/BibliotecarioService.java
@@ -4,9 +4,13 @@ import com.example.istateca.Clases.Bibliotecario;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface BibliotecarioService {
     @POST("crearbibliotecario")
     Call<Bibliotecario> addBibliotecario(@Body Bibliotecario bibliotecario);
+    @GET("bibliotecario/{id}")
+    Call<Bibliotecario> getBuscarDatos(@Path("id_bibliotecario") int id);
 }
